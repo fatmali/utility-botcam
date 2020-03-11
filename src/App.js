345046be179bb3ba8d5d3c920d2e25aace96aca7
Fatma Ali <alifatma2019@gmail.com>
2020-03-11 21:54:01 +0300

    fixed gh pages built

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import "./App.css";
-import WebcamCapture from "./components/webcam.component";
+import WebcamCapture from "./webcam.component";
 
 function App() {
   return (
