06b8f8a273fad13a43179949043ed7e897012bf1
Fatma Ali <alifatma2019@gmail.com>
2020-03-11 21:53:39 +0300

    Updates

---
+++ precache-manifest.5e4c0b168d1aff27de6258ff97581a9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880775ccf064bf6aed0e108dec4c1d73",
+    "url": "/utility-botcam/index.html"
+  },
+  {
+    "revision": "b86eb2d8ec006a0e6e7b",
+    "url": "/utility-botcam/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c46e30e6c6f732b84bb7",
+    "url": "/utility-botcam/static/js/2.23d91f64.chunk.js"
+  },
+  {
+    "revision": "fc546b2ab28544fdca15f51b30e06cdf",
+    "url": "/utility-botcam/static/js/2.23d91f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b86eb2d8ec006a0e6e7b",
+    "url": "/utility-botcam/static/js/main.e6590054.chunk.js"
+  },
+  {
+    "revision": "e7281ff987d504aba38b",
+    "url": "/utility-botcam/static/js/runtime-main.2f8bc78c.js"
+  }
+]);
